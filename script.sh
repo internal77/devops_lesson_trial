135ba13406442c95b6e305a297e9056798292ae7
Internal <73139884+internal77@users.noreply.github.com>
2021-09-28 17:51:10 +0300

    Add files via upload
    для AWS

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 -y
+sudo systemctl start apache2.service
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform" > /var/www/html/index.html
+echo "<br>font color="blue">Hello world" >> /var/www/html/index.html
